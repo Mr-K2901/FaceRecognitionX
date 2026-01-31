5405eeda453dd469cc555cf7f1fcca765e3091b0
Mr-K2901 <kapiljthakur2901@gmail.com>
2026-02-01 02:46:51 +0530

    CLAHE and Adjust Gamma: to enhance video streaming)

--- src/core/video_source.py
+++ src/core/video_source.py
@@ -2,6 +2,31 @@
 import cv2
 
 #-----------------------------------------------------------------------------------------------------------
+# CLAHE / Adjust Gamma
+#-----------------------------------------------------------------------------------------------------------
+
+import numpy as np
+def adjust_gamma(image, gamma=1.4):
+    inv_gamma = 1.0 / gamma
+    table = np.array(
+        [((i / 255.0) ** inv_gamma) * 255 for i in range(256)]
+    ).astype("uint8")
+    return cv2.LUT(image, table)
+
+def apply_clahe(image, clip_limit=2.0, grid_size=(8, 8)):
+    lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+    l, a, b = cv2.split(lab)
+
+    clahe = cv2.createCLAHE(
+        clipLimit=clip_limit,
+        tileGridSize=grid_size
+    )
+    l = clahe.apply(l)
+
+    lab = cv2.merge((l, a, b))
+    return cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+
+#-----------------------------------------------------------------------------------------------------------
 # Laptop Camera
 #-----------------------------------------------------------------------------------------------------------
 
--- src/main.py
+++ src/main.py
@@ -1,14 +1,32 @@
 # Entry point
 import cv2
-from core.video_source import LaptopCamera
+from core.video_source import LaptopCamera, adjust_gamma, apply_clahe
+
 
 def main():
     camera = LaptopCamera(camera_index=0)
     camera.start()
 
+
+    cv2.namedWindow(
+        "FaceRecognitionX - Laptop Camera",
+        cv2.WINDOW_NORMAL
+    )
+
+    cv2.setWindowProperty(
+        "FaceRecognitionX - Laptop Camera",
+        cv2.WND_PROP_FULLSCREEN,
+        cv2.WINDOW_FULLSCREEN
+    )
+
+
     try:
         while True:
+            #frame = camera.read()
             frame = camera.read()
+            frame = adjust_gamma(frame, gamma=1.5)
+            frame = apply_clahe(frame, clip_limit=2.0, grid_size=(8, 8))
+
             if frame is None:
                 continue
 
@@ -19,6 +37,7 @@ def main():
     finally:
         camera.stop()
         cv2.destroyAllWindows()
+        print("Camera stopped. Exiting program.")
 
 if __name__ == "__main__":
     main()
